f4316db8f5fa18266d005dbdc3847851414151ee
Jon Milner <email@jonmilner.com>
2016-05-10 17:22:54 -0400

    Add jquery and script to remove outline of buttons unless using keyboard

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,6 +1,12 @@
+import $ from 'jquery';
 import fastclick from 'fastclick';
 import cqProlyfill from 'cq-prolyfill';
+import removeOutline from './removeOutline';
 
 var cq = require('cq-prolyfill')({
   postcss: true
+});
+
+$(document).ready(function() {
+  removeOutline();
 });
